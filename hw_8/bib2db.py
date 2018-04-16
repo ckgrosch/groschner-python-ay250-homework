e5cc552afb342dbe15df480c9a7833ce5173b300
Kate Groschner <ckgroschner@gmail.com>
2018-04-15 19:29:54 -0700

    created bib upload and module to convert to db

---
+++ hw_8/bib2db.py
@@ -0,0 +1,106 @@
+from pybtex.database import parse_file
+from pybtex.richtext import String
+import sqlite3
+
+def citation_key(file):
+    """Takes in the collection, called file, after the pybtex function parse_file has already been called on it
+    and returns the citation keys for that collection"""
+    citation_keys = []
+    for entry in file.entries:
+        citation_keys.append(entry)
+    return citation_keys
+
+def author_list(file,citation_keys):
+    """Takes in the collection, called file, after the pybtex function parse_file has already been called on it
+    and returns the authors for that collection"""
+    authors = []
+    for entry in citation_keys:
+        auth_list = file.entries[entry].persons.get('author',[])
+        author_str = ""
+        for auth in auth_list:
+            author_str += str(auth).replace('{','').replace('}','')+" "
+        if author_str != "":
+            authors.append(author_str)
+        else:
+            authors.append("None")
+    return authors
+
+def volume_list(file,citation_keys):
+    """Takes in the collection, called file, after the pybtex function parse_file has already been called on it
+    and returns the volume numbers for that collection"""
+    volumes = []
+    for entry in citation_keys:
+        vol = file.entries[entry].fields.get('Volume',[])
+        if vol == []:
+            vol = 'None'
+        volumes.append(vol)
+    return volumes
+
+def title_list(file,citation_keys):
+    """Takes in the collection, called file, after the pybtex function parse_file has already been called on it
+    and returns the titles for that collection"""
+    volumes = []
+    for entry in citation_keys:
+        vol = file.entries[entry].fields.get('Title',[])
+        if vol == []:
+            vol = 'None'
+        else:
+            vol = vol.replace('{','').replace('}','')
+        volumes.append(vol)
+    return volumes
+
+def year_list(file,citation_keys):
+    """Takes in the collection, called file, after the pybtex function parse_file has already been called on it
+    and returns the years for that collection"""
+    volumes = []
+    for entry in citation_keys:
+        vol = file.entries[entry].fields.get('Year',[])
+        if vol == []:
+            vol = 'None'
+        volumes.append(vol)
+    return volumes
+
+def page_list(file,citation_keys):
+    """Takes in the collection, called file, after the pybtex function parse_file has already been called on it
+    and returns the pages for that collection"""
+    volumes = []
+    for entry in citation_keys:
+        vol = file.entries[entry].fields.get('Pages',[])
+        if vol == []:
+            vol = 'None'
+        volumes.append(vol)
+    return volumes
+
+def journal_list(file,citation_keys):
+    """Takes in the collection, called file, after the pybtex function parse_file has already been called on it
+    and returns the pages for that collection"""
+    volumes = []
+    for entry in citation_keys:
+        vol = file.entries[entry].fields.get('Journal',[])
+        if vol == []:
+            vol = 'None'
+        volumes.append(vol)
+    return volumes
+
+def bib_to_db(fname, collection_name, db_name):
+    """Takes in a .bib file and returns a sql database with the citation tag, author list, volume, journal,pages, year
+    and title"""
+    file = parse_file(fname)
+    citation_keys = citation_key(file)
+    authors = author_list(file,citation_keys)
+    volumes = volume_list(file,citation_keys)
+    journals = journal_list(file,citation_keys)
+    pages = page_list(file,citation_keys)
+    titles = title_list(file,citation_keys)
+    years = year_list(file,citation_keys)
+    clctn_list = [collection_name]*len(citation_keys)
+    rows = list(zip(citation_keys,authors,journals,volumes,pages,years,titles,clctn_list))
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    cmd1 = """DROP TABLE IF EXISTS {}""".format(collection_name)
+    c.execute(cmd1)
+    sql_cmd = """CREATE TABLE {} (id INTEGER PRIMARY KEY AUTOINCREMENT, citation_key TEXT, author_list TEXT, journal TEXT, volume TEXT, pages TEXT, year TEXT, title TEXT, collection TEXT)""".format(collection_name)
+    c.execute(sql_cmd)
+    c.executemany('insert into {} (citation_key,author_list,journal,volume,pages,year,title,collection) values (?,?,?,?,?,?,?,?)'.format(collection_name), rows)
+    conn.commit()
+    conn.close()
--- hw_8/hw_8.py
+++ hw_8/hw_8.py
@@ -1,7 +1,66 @@
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
+import os
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+from pybtex.database import parse_file
+from pybtex.richtext import String
+import sqlite3
+import os.path
+from bib2db import bib_to_db #bib2py is a set of functions I wrote to convert the .bib file to a .db
 
-@app.route('/')
-def butt_maker():
-    return render_template('index.html')
+#setting up flask
+app = Flask(__name__)
+#this section sets up all the directories for file upload
+if os.path.isdir('uploads'):
+    UPLOAD_FOLDER = 'uploads'
+else:
+    os.mkdir('uploads')
+    UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['bib'])
+db_name = 'uploads/uploaded_citations.db'
+
+#function for homepage
+@app.route('/', methods=['GET', 'POST'])
+def home_page():
+    con = sqlite3.connect(db_name)
+    cursor = con.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite%';")
+    collects_present = [clct[0] for clct in cursor.fetchall()]
+    return render_template('index.html', collects_present = collects_present)
+
+# All the functions for .bib file upload and file upload page
+def allowed_file(filename):
+    """This function is called by upload file to make sure only
+     .bib files are uploaded"""
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/insert', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            collection_name = request.form['collection_name']
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            fname = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            bib_to_db(fname,collection_name,db_name)
+            return redirect(url_for('home_page'))
+    return render_template('insert.html')
+# @app.route('/insert', methods=['GET', 'POST'])
+# def insert_stuff():
+#     return render_template('base.html', page_title = "Insert Stuff", content = "MAKE BUTTS")
+
+@app.route('/search', methods=['GET', 'POST'])
+def search_stuff():
+    return render_template('base.html', page_title = "Search Stuff", content = "BUTT FINDER")
